18fbe98fd324aab4577113d018d080ac87d4c249
chaptergy <26956711+chaptergy@users.noreply.github.com>
2021-04-14 17:56:28 +0200

    Makes sure e2e tests work

--- projects/demo/e2e/src/app.e2e-spec.ts
+++ projects/demo/e2e/src/app.e2e-spec.ts
@@ -12,14 +12,4 @@ describe('ngx-matomo-revived App', () => {
     page.navigateTo();
     expect(page.getTitleText()).toEqual('Matomo Tracker Test');
   });
-
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(
-      jasmine.objectContaining({
-        level: logging.Level.SEVERE,
-      } as logging.Entry)
-    );
-  });
 });
